ef752df38d0a7a3a650e571cd1098056e846e758
Eli Zibin <1131641+zibs@users.noreply.github.com>
2025-09-08 14:37:37 -0700

    Update version logging and message to v12

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Main module for optic-test
 export function getVersion() {
-  console.log('adds logging again v11 post branch -- edit v11 edit')
+  console.log('adds logging again v12 post branch -- edit v12 edit')
   console.log('latest edit')
 
   return '1.0.12'
 }
 
 export function getMessage() {
-  return 'Hello from optic-test release branch! test -- v11 -- editing again'
+  return 'Hello from optic-test release branch! test -- v12 -- editing again'
 }
 
 export default {
