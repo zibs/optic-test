af754dfcf9cc1c996f9032c2bc0a159302a3a5dd
Eli Zibin <1131641+zibs@users.noreply.github.com>
2025-09-08 14:34:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ export function getVersion() {
   console.log('adds logging again v11 post branch -- edit v11 edit')
   console.log('latest edit')
 
-  return '1.0.11'
+  return '1.0.12'
 }
 
 export function getMessage() {
