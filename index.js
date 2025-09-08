f44f80661afafb98788162762ce3f3d34514a86d
Eli Zibin <1131641+zibs@users.noreply.github.com>
2025-09-08 14:47:47 -0700

    Update version and message for v14 release

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Main module for optic-test
 export function getVersion() {
-  console.log('adds logging again v13 post branch -- edit v13 edit')
+  console.log('adds logging again v14 post branch -- edit v14 edit')
   console.log('latest edit')
 
   return '1.0.14'
 }
 
 export function getMessage() {
-  return 'Hello from optic-test release branch! test -- v13 -- editing again'
+  return 'Hello from optic-test release branch! test -- v14 -- editing again'
 }
 
 export default {
