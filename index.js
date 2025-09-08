9d2b1ee2fcd459013c7dfebf842f9c967af0c8e7
Eli Zibin <1131641+zibs@users.noreply.github.com>
2025-09-08 13:18:43 -0700

    Update logging and message strings in index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Main module for optic-test
 export function getVersion() {
-  console.log('adds logging again v5 post branch')
+  console.log('adds logging again v5 post branch -- edit v5 edit')
 
   return '1.0.5'
 }
 
 export function getMessage() {
-  return 'Hello from optic-test release branch! test -- v5'
+  return 'Hello from optic-test release branch! test -- v5 -- editing again'
 }
 
 export default {
