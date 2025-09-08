1648f3cd773401631ab52b33650f5ed18bf0b868
Eli Zibin <1131641+zibs@users.noreply.github.com>
2025-09-08 11:15:41 -0700

    feat: test

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Main module for optic-test
 export function getVersion() {
+  console.log('adds logging')
+
   return '1.0.1'
 }
 
 export function getMessage() {
-  return 'Hello from optic-test release branch!'
+  return 'Hello from optic-test release branch! test'
 }
 
 export default {
