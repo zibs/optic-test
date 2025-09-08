57ddf0d439b62049092194fff478721e0a2dcf78
Eli Zibin <1131641+zibs@users.noreply.github.com>
2025-09-08 14:05:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Main module for optic-test
 export function getVersion() {
   console.log('adds logging again v6 post branch -- edit v6 edit')
+  console.log('latest edit')
 
-  return '1.0.5'
+  return '1.0.6'
 }
 
 export function getMessage() {
